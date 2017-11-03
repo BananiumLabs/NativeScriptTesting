1cad8b5969f525ae3458b56c7c870c4e22702b67
Ben Cuan <ben.cuan@gmail.com>
2017-11-03 16:58:29 -0700

    Angularize Demo Template
    Wups had some issues and had to rebase.

---
+++ app/tabs/graph-demo/graph-demo.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+    selector: "GraphDemo",
+    moduleId: module.id,
+    templateUrl: "./graph-demo.component.html"
+})
+export class GraphDemoComponent implements OnInit {
+    constructor() {
+        /* ***********************************************************
+        * Use the constructor to inject services.
+        *************************************************************/
+    }
+
+    ngOnInit(): void {
+        /* ***********************************************************
+        * Use the "ngOnInit" handler to initialize data for the view.
+        *************************************************************/
+    }
+}
